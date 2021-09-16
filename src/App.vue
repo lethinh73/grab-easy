f10437bcded826e5168fb4091fe9185f87518cda
Thinh Le <lethinh73@hotmail.com>
2021-09-16 02:11:07 -0500

    update styles and favicon

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
     <img class="logo" src="./images/grabeasy.png" alt="logo"/>
     <div class="fs-3 intro">Hey there, Let's get started.<p>Enter your mobile number</p></div>
 
-    <div class="bottom-container"><div class="phone-box"><span>+1</span><input class="tel-input" type="tel" placeholder="Enter your mobile number"></div></div>
+    <div class="bottom-container">
+      <div class="phone-box">
+        <span>+1</span><input class="tel-input" type="tel" placeholder="Enter your mobile number">
+
+      </div>
+      <a href="/" ><img class="enter-btn" src="./images/doubleleft.svg" alt="enter-btn"/></a>
+    </div>
 
   </div>
 </template>
@@ -75,5 +81,9 @@ body {
   letter-spacing: 0.19px;
   color: #FFFFFF;
   opacity: 0.5;
+}
+
+.enter-btn {
+  margin-left: 30px;
 }
 </style>
