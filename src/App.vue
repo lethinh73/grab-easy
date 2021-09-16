1f1925803cd63e4479ee4a148ff9343587bd828c
Thinh Le <lethinh73@hotmail.com>
2021-09-16 10:52:12 -0500

    update styles

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="top-container">
-    <img class="logo" src="./images/grabeasy.png" alt="logo"/>
-    <div class="fs-3 intro">Hey there, Let's get started.<p>Enter your mobile number</p></div>
+    <img class="logo" src="./images/grabeasy.png" alt="logo" />
+    <div class="fs-3 intro">
+      Hey there, Let's get started.
+      <p>Enter your mobile number</p>
+    </div>
 
     <div class="bottom-container">
       <div class="phone-box">
-        <span>+1</span><input class="tel-input" type="tel" placeholder="Enter your mobile number">
-
+        <span>+1</span
+        ><input
+          class="tel-input"
+          type="tel"
+          placeholder="Enter your mobile number"
+        />
       </div>
-      <a href="/" ><img class="enter-btn" src="./images/doubleleft.svg" alt="enter-btn"/></a>
+      <a href="/"
+        ><img class="enter-btn" src="./images/doubleleft.svg" alt="enter-btn"
+      /></a>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
@@ -26,12 +34,17 @@ export default {
 }
 
 body {
-  margin: 0;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   background-color: black;
 }
 
 .top-container {
-  margin-top: 10%;
+  margin-top: 8vh;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -46,7 +59,8 @@ body {
 }
 
 .intro {
-  margin-top: 250px;
+  margin-top: 30vh;
+  margin-bottom: 5px;
   text-align: left;
   width: 310px;
 }
@@ -54,7 +68,10 @@ body {
 .bottom-container {
   display: flex;
   width: 310px;
+  height: 100%;
   justify-content: flex-start;
+  bottom: 0ch;
+  margin-bottom: 0;
 }
 
 .phone-box {
@@ -63,9 +80,9 @@ body {
   align-items: center;
   justify-content: flex-start;
   padding-left: 10px;
-  height: 30px;
-  width: 78%;
-  border: 1px solid #FFFFFF;
+  height: 40px;
+  width: 80%;
+  border: 1px solid #ffffff;
   border-radius: 24px;
   opacity: 1;
 }
@@ -74,16 +91,17 @@ body {
   font-family: "Roboto", sans-serif;
   font-size: 1rem;
   color: white;
-  margin-left: 10px;
+  margin-left: 5px;
   border: hidden;
-  width: 12rem;
+  width: 13rem;
   background-color: black;
   letter-spacing: 0.19px;
-  color: #FFFFFF;
+  color: #ffffff;
   opacity: 0.5;
 }
 
 .enter-btn {
+  margin-top: 10px;
   margin-left: 30px;
 }
 </style>
