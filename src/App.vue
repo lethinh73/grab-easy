85a51e15b2a45d742a0128ab8a61163849eaf379
Thinh Le <lethinh73@hotmail.com>
2021-09-16 02:04:27 -0500

    starter files and first page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="top-container">
+    <img class="logo" src="./images/grabeasy.png" alt="logo"/>
+    <div class="fs-3 intro">Hey there, Let's get started.<p>Enter your mobile number</p></div>
+
+    <div class="bottom-container"><div class="phone-box"><span>+1</span><input class="tel-input" type="tel" placeholder="Enter your mobile number"></div></div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  background-color: black;
+}
+
+.top-container {
+  margin-top: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Roboto", sans-serif;
+  color: white;
+}
+
+.logo {
+  height: fit-content;
+  width: fit-content;
+}
+
+.intro {
+  margin-top: 250px;
+  text-align: left;
+  width: 310px;
+}
+
+.bottom-container {
+  display: flex;
+  width: 310px;
+  justify-content: flex-start;
+}
+
+.phone-box {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  padding-left: 10px;
+  height: 30px;
+  width: 78%;
+  border: 1px solid #FFFFFF;
+  border-radius: 24px;
+  opacity: 1;
+}
+
+.tel-input {
+  font-family: "Roboto", sans-serif;
+  font-size: 1rem;
+  color: white;
+  margin-left: 10px;
+  border: hidden;
+  width: 12rem;
+  background-color: black;
+  letter-spacing: 0.19px;
+  color: #FFFFFF;
+  opacity: 0.5;
 }
 </style>
