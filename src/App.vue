f6d52e4d7a91efa38105c6d80f5841ce4442c74b
Thinh Le <lethinh73@hotmail.com>
2021-09-19 12:36:59 -0500

    update styles

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="container-fluid h-100">
-    <div class="row justify-content-center">
-      <div class="col-4">
-        <img class="col-12" src="./images/grabeasy.png" alt="logo" />
+  <div class="view container-fluid">
+    <!-- Logo section -->
+    <div class="logo row">
+      <div class="col">
+        <img src="./images/grabeasy.png" alt="logo" />
       </div>
     </div>
+    <!-- End logo section -->
 
-    <div class="row justify-content-center">
-      <div class="row justify-content-center">
-        <div class="col-4 flex-start">
-          <p style="">
-            Hey there, Let's get started.<br />Enter your mobile number
-          </p>
+    <!-- Bottom section -->
+    <div class="bottom row">
+      <!-- Text section -->
+      <div class="text row">
+        <div class="text col">
+          <p>Hey there, Let's get started.<br />Enter your mobile number</p>
         </div>
       </div>
+      <!-- End text section -->
 
-      <div class="row justify-content-center">
-        <div class="col-4 flex-start">
+      <!-- Input section -->
+      <div class="input row">
+        <div class="input col">
           <input type="tel" />
           <a href="/"
             ><img class="btn" src="./images/doubleleft.svg" alt="enter-btn"
           /></a>
         </div>
       </div>
+      <!-- End input section -->
     </div>
+    <!-- End bottom section -->
   </div>
 </template>
 
@@ -43,5 +49,26 @@ body {
   color: white;
   margin: 0;
   background-color: black;
+}
+
+.view {
+  position: relative;
+  height: 100vh;
+  width: 100%;
+}
+
+.logo.row {
+  position: absolute;
+  top: 100px;
+  width: 100%;
+  text-align: center;
+}
+
+.bottom.row {
+  position: absolute;
+  top: auto;
+  bottom: 100px;
+  width: 100%;
+  text-align: center;
 }
 </style>
