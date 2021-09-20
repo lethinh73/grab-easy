b19b1b6338831e41c6edacd14deecbf36cf76cee
Thinh Le <lethinh73@hotmail.com>
2021-09-20 01:11:09 -0500

    fixing chat function

--- src/App.vue
+++ src/App.vue
@@ -41,23 +41,38 @@
 </template>
 
 <script>
+let step = 1;
+let phoneNumber = 0;
+
 export default {
   name: "App",
 
   methods: {
     addChat: function() {
-      let chatContent = document.querySelector("#input-content").value;
-      document.querySelector("#chat-screen").innerHTML +=
-        "<p style='padding-left: 150px; color: yellow'>" + chatContent + "</p>";
-      document.querySelector("#input-content").value = "";
-      this.answerChat(1);
-    },
-    answerChat: function(step) {
-      if (step == 1) {
-        let chatContent = "Please enter your name:";
-        document.querySelector("#chat-screen").innerHTML +=
-          "<p>" + chatContent + "</p>";
+      console.log(step);
+      if (step === 1) {
+        phoneNumber = document.querySelector("#input-content").value;
+        this.printUserChat();
+        this.printBotChat("Enter the received OTP on +1" + phoneNumber);
+        step++;
+      } else if (step === 2) {
+        this.printUserChat();
+        this.printBotChat("We are working on this!!!");
+        step++;
+      } else if (step === 3) {
+        this.printUserChat();
+        this.printBotChat("We are working on this!!!");
       }
+    },
+    printUserChat: function() {
+      let userChat = document.querySelector("#input-content").value;
+      document.querySelector("#chat-screen").innerHTML +=
+        "<p style='padding-left: 150px; color: yellow'>" + userChat + "</p>";
+      document.querySelector("#input-content").value = "";
+    },
+    printBotChat: function(content) {
+      document.querySelector("#chat-screen").innerHTML +=
+        "<p>" + content + "</p>";
     },
   },
 };
