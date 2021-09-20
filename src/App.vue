fcc62c3f2ff7390c9e3bfca570ef4f67c8023a11
Thinh Le <lethinh73@hotmail.com>
2021-09-20 00:42:47 -0500

    added chat box feature

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,15 @@
     <!-- End logo section -->
 
     <div class="middle row">
-      <h1>Information here</h1>
+      <div class="text row">
+        <div id="chat-screen" class="text col">
+          <p>Hey there, Let's get started.<br />Enter your mobile number</p>
+        </div>
+      </div>
     </div>
 
     <!-- Bottom section -->
     <div class="bottom row">
-      <!-- Text section -->
-      <div class="text row">
-        <div class="text col">
-          <p>Hey there, Let's get started.<br />Enter your mobile number</p>
-        </div>
-      </div>
-      <!-- End text section -->
-
       <!-- Input section -->
       <div class="input row">
         <div class="input col">
@@ -29,11 +25,13 @@
             <span id="area-code">
               +1
             </span>
-            <input type="tel" />
+            <input id="input-content" type="tel" />
           </div>
-          <a href="/"
-            ><img class="btn" src="./images/doubleleft.svg" alt="enter-btn"
-          /></a>
+          <button
+            class="enter-btn"
+            v-on:click="addChat()"
+            v-on:keydown.enter="addChat()"
+          ></button>
         </div>
       </div>
       <!-- End input section -->
@@ -45,6 +43,23 @@
 <script>
 export default {
   name: "App",
+
+  methods: {
+    addChat: function() {
+      let chatContent = document.querySelector("#input-content").value;
+      document.querySelector("#chat-screen").innerHTML +=
+        "<p style='padding-left: 150px; color: yellow'>" + chatContent + "</p>";
+      document.querySelector("#input-content").value = "";
+      this.answerChat(1);
+    },
+    answerChat: function(step) {
+      if (step == 1) {
+        let chatContent = "Please enter your name:";
+        document.querySelector("#chat-screen").innerHTML +=
+          "<p>" + chatContent + "</p>";
+      }
+    },
+  },
 };
 </script>
 
