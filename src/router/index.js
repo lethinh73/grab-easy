7c2e72cf26ed88bca8de9f86ca6fe0145580af6b
Victor Ngo <Victor.Ngo@labs196.com>
2021-09-22 11:01:45 -0500

    added Offers Page

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../pages/Login.vue";
 import Venue from "../pages/Venue.vue";
 import Items from "../pages/Items.vue";
+import Offers from "../pages/Offers.vue";
 
 const routes = [
   { path: "/", name: "Login", component: Login },
   { path: "/venue", name: "Venue", component: Venue },
   { path: "/items", name: "Items", component: Items },
+  { path: "/offers", name: "Offers", component: Offers },
 ];
 
 const router = createRouter({
