d63bf2f78079988408ec1270c5e86efe5b2c9f65
Thinh Le <58018327+lethinh73@users.noreply.github.com>
2021-09-22 12:00:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 5000;
 app.listen(port);
