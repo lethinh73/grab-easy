e42d7af84e55b1cd82c56e8d5e9a0d6c74115de2
Thinh Le <58018327+lethinh73@users.noreply.github.com>
2021-09-22 11:52:38 -0500

    Update server.js
    test heroku deploy 2

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
