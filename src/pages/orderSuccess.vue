6c9e56a48fe4fa2fa238fc08618937e302fca498
Nung Khual <nung.khual@labs196.com>
2021-09-23 13:14:35 -0500

    order Success

---
+++ src/pages/orderSuccess.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container">
+        <div class="check"/>
+        <br>
+        <div class="tex">
+            Your order is placed successfully
+        </div>
+    </div>
+</template>
+
+<style>
+*{
+  box-sizing: border-box;
+}
+.container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 350px !important;
+    height: 300px;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+    box-shadow: 2px 3px 6px 2px #00000029;
+    border-radius: 41px;
+    opacity: 1;
+    position: fixed;
+    left: calc(50% - 175px);
+    bottom: 200px;
+}
+
+.check{
+    top: 228px;
+    left: 140px;
+    width: 79px;
+    height: 79px;
+    background: transparent url('../images/icon-check-alt2.svg') 0% 0% no-repeat padding-box;
+    opacity: 1;
+}
+
+.tex{
+    top: 322px;
+    left: 45px;
+    width: 270px;
+    height: 103px;
+    text-align: center;
+    font: normal normal bold 18px/24px Roboto;
+    letter-spacing: 0.32px;
+    color: #0A1F44;
+    opacity: 1;
+}
+
+@media (max-width:768px){
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 350px !important;
+        height: 300px;
+        background: #FFFFFF 0% 0% no-repeat padding-box;
+        box-shadow: 0px 3px 6px #00000029;
+        border-radius: 41px;
+        opacity: 1;
+        position: fixed;
+        left: calc(50% - 175px);
+        bottom: 400px;
+}
+}
+
+@media (max-width: 1024px){
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 350px !important;
+        height: 300px;
+        background: #FFFFFF 0% 0% no-repeat padding-box;
+        box-shadow: 2px 3px 6px 2px #00000029;
+        border-radius: 41px;
+        opacity: 1;
+        position: fixed;
+        left: calc(50% - 175px);
+        bottom: 550px;
+}
+
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Venue from "../pages/Venue.vue";
 import GoogleMap from '../pages/GoogleMap.vue'
 import Items from "../pages/Items.vue";
 import Offers from "../pages/Offers.vue";
-
+import orderSuccess from "../pages/orderSuccess.vue"
 
 const routes = [
   { path: "/", name: "Login", component: Login },
@@ -12,6 +12,7 @@ const routes = [
   { path: "/GoogleMap", name: "Map", component: GoogleMap },
   { path: "/items", name: "Items", component: Items },
   { path: "/offers", name: "Offers", component: Offers },
+  { path: "/orderSuccess", name: "orderSuccess", component: orderSuccess }
 ];
 
 const router = createRouter({
