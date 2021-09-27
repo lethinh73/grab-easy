21cf30cac15ee426328a9bc047812274cb7d2957
Thinh Le <lethinh73@hotmail.com>
2021-09-27 01:30:27 -0500

    working on Profile

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="profile-container" style="background-color: green">
-    <div class="greeting">
-      Hello, Thinh
+  <div class="profile-container">
+    <div class="top">
+      <button class="btn">
+        <i class="fas fa-arrow-left"></i>
+      </button>
+      <div class="greeting">Hello, Grab Easy!</div>
+    </div>
+
+    <div class="middle">
+      <button class="btn menu">Notifications</button>
+      <button class="btn menu">Address</button>
+      <button class="btn menu">Log out</button>
     </div>
     <Navbar class="nav" />
   </div>
@@ -33,6 +42,32 @@ body {
   position: absolute;
   width: 100%;
   height: 100%;
+  background-color: orange;
+}
+
+.top {
+  margin: 50px auto auto 30px;
+  border: 1px black solid;
+}
+
+.fa-arrow-left {
+  font-size: 25px;
+}
+
+.greeting {
+  font-size: 30px;
+}
+
+.middle {
+  border: 1px solid black;
+  margin: 50px auto auto 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+}
+
+.menu {
+  border: 1px solid black;
 }
 
 .nav {
