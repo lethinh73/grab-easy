2e6b72ddb2df9c37a88c6f0a7e8c775fb704fd55
Thinh Le <lethinh73@hotmail.com>
2021-09-30 17:26:00 -0500

    update styling

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -114,7 +114,8 @@ export default {
 .view {
   position: absolute;
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  min-height: 640px;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
@@ -134,12 +135,12 @@ export default {
 }
 
 .middle.row {
-  /* border: 1px white solid; */
+  border: 1px white solid;
   position: fixed;
   display: flex;
   flex-direction: column-reverse;
   width: 100%;
-  max-height: 60vh;
+  max-height: 60%;
   overflow: auto;
   -ms-overflow-style: none;
   scrollbar-width: none;
