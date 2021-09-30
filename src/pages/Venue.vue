4a7e74cb7523759bd7c2ade609f79dae8669688a
Chloe <chloe.ng.1292@gmail.com>
2021-09-30 11:04:34 -0500

    fixed

--- src/pages/Venue.vue
+++ src/pages/Venue.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="wrapper">
     <SearchBar class="fixed-top" />
     <!-- Carousel part -->
@@ -53,46 +52,10 @@
           />
         </div>
       </div>
-
-  <div class="search">
-    <SearchBar />
-  </div>
-  <!-- Carousel part -->
-  <div
-    id="carouselExampleIndicators"
-    class="carousel slide d-flex justify-content-center container mt-5"
-    data-bs-ride="carousel"
-  >
-    <div class="carousel-indicators">
-      <button
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide-to="0"
-        class="active"
-        aria-current="true"
-        aria-label="Slide 1"
-      ></button>
-
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
     </div>
-
+    <div class="search">
+      <SearchBar />
+    </div>
     <!-- Cannot find part -->
     <div class="cannot-find container mt-5">
       <p class="text-center rounded-pill my-2">
@@ -116,62 +79,10 @@
           <p class="mt-2 mb-1">{{ place.name }}</p>
           <p class="area-name text-secondary">{{ place.area }}</p>
         </div>
-
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="../images/main-store/lg.png" class="d-block w-100" alt="..." />
-      </div>
-      <div class="carousel-item">
-        <img src="../images/main-store/lg.png" class="d-block w-100" alt="..." />
-      </div>
-      <div class="carousel-item">
-        <img src="../images/main-store/lg.png" class="d-block w-100" alt="..." />
-      </div>
-    </div>
-    <button
-      class="carousel-control-prev"
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide="prev"
-    >
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button
-      class="carousel-control-next"
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide="next"
-    >
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-  <!-- Cannot find part -->
-  <div class="cannot-find container mt-5">
-    <p class="text-center rounded-pill my-2">
-      Cannot find any Creamston outlet near you, we are available at the below locations.
-    </p>
-  </div>
-  <div class="order-at container">
-    <p class="fw-bold my-2">
-      You can order using app at:
-    </p>
-  </div>
-  <div class=" container">
-    <div class=" scrolling-wrapper d-flex flex-nowrap ">
-      <div class="card-wrapper col-3 mb-3 me-3" v-for="place in places" :key="place.id">
-        <img :src="require(`../images/place-name/${place.img}`)" />
-        <p class="mt-2 mb-1">{{ place.name }}</p>
-        <p class="area-name text-secondary">{{ place.area }}</p>
-
       </div>
     </div>
     <Navbar class="fixed-bottom" />
   </div>
-
-  <Navbar class="nav" />
-
 </template>
 <script scoped>
 import Navbar from "../components/Navbar.vue";
