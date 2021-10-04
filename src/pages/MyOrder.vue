0a4684e7fa9cf6b8337767ab5dd1204b49e60eb7
Thinh Le <lethinh73@hotmail.com>
2021-10-04 11:16:49 -0500

    working on myorders page/ ongoing section

--- src/pages/MyOrder.vue
+++ src/pages/MyOrder.vue
@@ -37,6 +37,18 @@
         <div class="tab-pane fade show active" id="ongoing-order" role="tabpanel" aria-labelledby="ongoing-order-tab">
           <img src="../images/myorders/ongoing.svg" style="width: 200px; height: 200px;" alt="ongoing" />
           <h3>Order now for your orders to appear here.</h3>
+          <div class="card w-90">
+            <div class="row">
+              <div class="col-3">
+                <img src="../images/myorders/pizza.png" style="width: 90px; height: auto;" alt="pizza" />
+              </div>
+              <div class="col-9">
+                <h3>The Pizza Place</h3>
+                <p>Ordered Time : 20:36 | Order ID : 3456 Items : Cheese Garlic Bread</p>
+                <button class="btn-status">Status</button>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="tab-pane fade" id="past-order" role="tabpanel" aria-labelledby="past-order-tab">
           <img src="../images/myorders/past.svg" style="width: 200px; height: 200px;" alt="ongoing" />
@@ -126,6 +138,26 @@ ul.nav-pills {
   padding-top: 15%;
   width: 100%;
   text-align: center;
+  overflow: auto;
+}
+
+.card {
+  position: relative;
+  border-radius: 16px;
+  box-shadow: 0px 3px 6px #00000029;
+  height: fit-content;
+  padding: 10px;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: center;
+  margin-bottom: 30px;
+  font-size: 16px;
+}
+
+.col-9 p {
+  color: #616b71;
 }
 
 .navbar {
